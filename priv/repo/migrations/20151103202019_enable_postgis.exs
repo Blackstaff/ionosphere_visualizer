c4dfdee11cf4599b7f9d5e39021ebf1b9413c77e
Mateusz Czarnecki <mateusz.czarnecki92@gmail.com>
2015-11-03 22:47:02 +0100

    Add migration for enabling PostGIS

---
+++ priv/repo/migrations/20151103202019_enable_postgis.exs
@@ -0,0 +1,11 @@
+defmodule IonosphereVisualizer.Repo.Migrations.EnablePostgis do
+  use Ecto.Migration
+
+  def up do
+  	execute "CREATE EXTENSION IF NOT EXISTS postgis"
+  end
+
+  def down do
+  	execute "DROP EXTENSION IF EXISTS postgis"
+  end
+end

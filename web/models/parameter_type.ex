2268bf6aeee7cf2507f6d84d6230d4f51778123c
Mateusz Czarnecki <mateusz.czarnecki92@gmail.com>
2016-02-09 00:33:34 +0100

    Add Polish translation

--- web/models/parameter_type.ex
+++ web/models/parameter_type.ex
@@ -1,30 +1,32 @@
 defmodule IonosphereVisualizer.ParameterType do
+  import IonosphereVisualizer.Gettext
+
   defstruct name: "", long_name: "", unit: ""
 
   @types [
     %{
       name: "foF2",
-      long_name: "F2 layer critical frequency",
+      long_name: gettext("F2 layer critical frequency"),
       unit: "MHz"
     },
     %{
       name: "foE",
-      long_name: "E layer critical frequency",
+      long_name: gettext("E layer critical frequency"),
       unit: "MHz"
     },
     %{
       name: "hpF",
-      long_name: "Minimum virtual height of F trace",
+      long_name: gettext("Minimum virtual height of F trace"),
       unit: "km"
     },
     %{
       name: "hmF2",
-      long_name: "Peak height of F2 layer",
+      long_name: gettext("Peak height of F2 layer"),
       unit: "km"
     },
     %{
       name: "MUF3000",
-      long_name: "Maximum usable frequency",
+      long_name: gettext("Maximum usable frequency"),
       unit: "MHz"
     }
   ]
@@ -34,12 +36,14 @@ defmodule IonosphereVisualizer.ParameterType do
   end
 
   def get_types do
-    for type <- @types, do: struct(__MODULE__, type)
+    for type <- @types, do: struct(__MODULE__, Map.put(type, :long_name,
+      Gettext.gettext(IonosphereVisualizer.Gettext, type.long_name)))
   end
 
   def get(name) do
     type = @types
     |> Enum.find(&(&1.name == name))
-    struct(__MODULE__, type)
+    struct(__MODULE__, Map.put(type, :long_name,
+      Gettext.gettext(IonosphereVisualizer.Gettext, type.long_name)))
   end
 end
